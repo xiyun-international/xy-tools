0a59be84c04da61615845f81f80baff13556dbbb
wubaiqing (琉璃) <wubaiqing@vip.qq.com>
2019-04-24 16:43:35 +0800

    pref: 优化变量命名规范

--- src/build.ts
+++ src/build.ts
@@ -3,17 +3,19 @@ import { IOpts } from './types';
 import { readdirSync } from 'fs';
 import { isLerna } from './utils';
 
-import buildCss from './build/theme';
+import buildTheme from './build/theme';
 import buildComponent from './build/component';
 
 export default async function(opts: IOpts) {
   const { cwd, cmd } = opts;
 
-  const build = cmd === 'theme' ? buildCss : buildComponent;
+  const build = cmd === 'theme' ? buildTheme : buildComponent;
 
   if (isLerna(cwd)) {
     const dirs = readdirSync(join(cwd, 'packages'));
-    dirs.forEach(pkg => build(`./packages/${pkg}`, { cwd }));
+    dirs.forEach(pkg => {
+      build(`./packages/${pkg}`, { cwd });
+    });
   } else {
     build('./', { cwd });
   }
